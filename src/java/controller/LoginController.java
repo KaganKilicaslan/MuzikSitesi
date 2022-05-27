9f05622d8c28c01dfc0c04206d1279f04441eab3
senanurpaksoy <senapksy@gmail.com>
2022-05-27 14:01:47 +0300

    validation işlemi uygulandı login güncellendi

--- src/java/controller/LoginController.java
+++ src/java/controller/LoginController.java
@@ -8,8 +8,11 @@ import entity.Kullanici;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.faces.application.FacesMessage;
+import jakarta.faces.component.UIComponent;
 import jakarta.faces.context.FacesContext;
+import jakarta.faces.validator.ValidatorException;
 import java.io.Serializable;
+import static javax.management.Query.value;
 
 /**
  *
@@ -17,22 +20,40 @@ import java.io.Serializable;
  */
 @Named(value = "loginController")
 @SessionScoped
-public class LoginController implements Serializable{
+public class LoginController implements Serializable {
 
     private Kullanici user;
+
     public LoginController() {
     }
-    
-    public void login(){
-        if(user.getNickName().equals("test")&& user.getSifre().equals("123") ){
+
+    public void login() {
+        if (user.getNickName().equals("test") && user.getSifre().equals("12345678")) {
             FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("validUser", user);
         } else {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("KULLANICI ADI VEYA ŞİFRE YANLIŞ!!"));
         }
     }
+
+    public boolean validatePassword(FacesContext context, UIComponent cmp, Object value) throws ValidatorException {
+        String v = (String) value;
+
+        if (v.isEmpty()) {
+            throw new ValidatorException(new FacesMessage("Şifre alanı boş olamaz!"));
+        } else if (v.length() < 7) {
+            throw new ValidatorException(new FacesMessage("Şifre alanı 8 karakterden kısa olamaz!"));
+        }
+
+        return true;
+    }
+
+    public void register() {
+        System.out.println("-------REGISTRED--------");
+    }
+
     public Kullanici getUser() {
-        if(user == null){
-            user= new Kullanici();
+        if (user == null) {
+            user = new Kullanici();
         }
         return user;
     }
@@ -40,5 +61,5 @@ public class LoginController implements Serializable{
     public void setUser(Kullanici user) {
         this.user = user;
     }
-    
+
 }
